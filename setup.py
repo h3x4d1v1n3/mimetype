33f79b4a6d076410f88a21203e7d2bb63b5214bc
h3x4d1v1n3 <h3x4d1v1n3@gmail.com>
2022-02-04 04:04:54 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mimetype', 
+    description='complete collection of extentions and their mimetypes', 
+    version='1.0',
+    packages=find_packages()
+)
